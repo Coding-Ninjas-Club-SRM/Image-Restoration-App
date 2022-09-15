dbb19152d39b844af1ccbe11cf7c787c10028b5f
ChetasShree <cm7310@srmist.edu.in>
2022-09-15 13:33:16 +0530

    🚀Added previous images page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_restoration/screens/home_page.dart';
+import 'package:image_restoration/screens/previous%20_images.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: HomePage.name,
-      routes: {HomePage.name: (context) => HomePage()},
+      routes: {HomePage.name: (context) => PreviousImagesPage()},
     );
   }
 }
